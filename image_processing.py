71ce5ecefa7711380ff3025b4bfa6896a1c14f91
Jeremy Arsenault <arsena21@msu.edu>
2020-12-04 18:32:15 -0500

    robot calibration

--- image_processing.py
+++ image_processing.py
@@ -1,15 +1,17 @@
 import cv2
 import numpy as np
 import imutils
+import math
+import time
 
 class Camera:
     """
     Class to handle all image processing
     """
-    def __init__(self, device=0):
+    def __init__(self, device=1):
         # TABLE SIZE
-        self.table_width = 1.5
-        self.table_length = 2.7
+        self.table_width = 1
+        self.table_length = 2
         self.rescaled_dim = (150, 270)
         
         # IMPORTANT COLOR CODES:
@@ -25,7 +27,30 @@ class Camera:
         # set corners
         self.set_corners()
         
-    def color_mask(self, img, color, thresh=10, blur=3):
+    def get_image(self):
+        """
+        get image from camera
+        :return: raw image, perspective shifted image
+        """
+        self.flush_buffer()
+        _, frame = self.cam.read()
+        shift_frame = self.perspective_shift(frame)
+        return frame, shift_frame
+    
+    def flush_buffer(self):
+        """
+        flush buffer and update clock
+        open cv is annoying
+        """
+        t1 = time.time()
+        while True:
+            t2 = time.time()
+            if t2-t1>0.03:
+                break
+            t1 = t2
+            self.cam.read()
+        
+    def color_mask(self, img, color, thresh=20, blur=3):
         """
         Mask image according to color
         :return: mask (0:other colors, 1:selected color)
@@ -45,7 +70,7 @@ class Camera:
                         [self.rescaled_dim[0]-1, self.rescaled_dim[1]-1],
                         [0, self.rescaled_dim[1]-1]])
         # compute the perspective transform matrix and then apply it
-        M = cv2.getPerspectiveTransform(self.corners, dst)
+        M = cv2.getPerspectiveTransform(np.float32(self.corners), np.float32(dst))
         warped = cv2.warpPerspective(img, M, self.rescaled_dim)
 
         return warped
@@ -66,8 +91,8 @@ class Camera:
         areas = []
         for c in contours:
             M = cv2.moments(c)
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
+            cX = int(M["m10"] / max(M["m00"],1))
+            cY = int(M["m01"] / max(M["m00"],1))
             centers.append([cX,cY])
             areas.append(cv2.contourArea(c))
             
@@ -88,8 +113,9 @@ class Camera:
         """
         Get the corners of the table for perspective shifts
         """
+        self.flush_buffer()
         _, frame = self.cam.read()
-        corner_mask = self.color_mask(frame, self.color_pink)
+        corner_mask = self.color_mask(frame, self.color_pink, thresh=30, blur=5)
         pts = self.find_centroids(corner_mask, 4)
         
         # put pts in correct order
@@ -103,7 +129,7 @@ class Camera:
         
         self.corners = corners
         
-    def get_pos(self, loc):
+    def abs_to_meter(self, loc):
         """
         convert location of centroid into absolute position in meters
         :return: position
@@ -111,28 +137,49 @@ class Camera:
         pos_y = (self.table_length/self.rescaled_dim[1])*(loc[0]-self.rescaled_dim[1]/2)
         pos_x = (self.table_width/self.rescaled_dim[0])*(loc[1]-self.rescaled_dim[0]/2)
         return [pos_x, pos_y]
+    
+    def get_pos(self, frame):
+        """
+        find position state vector or everything on table
+        :return: [puck pos, s1 pos, s2 pos]
+        """
+        frame = self.perspective_shift(frame)
+        
+        puck_mask = self.color_mask(frame, self.color_green, thresh=15)
+        striker_mask = self.color_mask(frame, self.color_orange)
+        
+        puck_loc = self.find_centroids(puck_mask)
+        striker_locs = self.find_centroids(striker_mask, 2)
+        
+        p_pos = self.abs_to_meter(puck_loc[0])
+        if striker_locs[0][1]<striker_locs[1][1]:
+            s1_pos = self.abs_to_meter(striker_locs[0])
+            s2_pos = self.abs_to_meter(striker_locs[1])
+        else:
+            s1_pos = self.abs_to_meter(striker_locs[0])
+            s2_pos = self.abs_to_meter(striker_locs[1])            
+        
+        return np.array([p_pos, s1_pos, s2_pos])
         
     def get_state(self):
         """
         Capture frame and find position state vector or everything on table
         :return: [puck pos, s1 pos, s2 pos]
         """
-        _, frame = self.cam.read()
+        self.flush_buffer()
+        _, frame1 = self.cam.read()
+        t = time.time()
+        p1 = self.get_pos(frame1)
         
-        frame = self.perspective_shift(frame)
+        _, frame2 = self.cam.read()
+        elapsed = time.time()-t
+        t = time.time()
+        p2 = self.get_pos(frame2)
         
-        puck_mask = self.color_mask(frame, self.color_green)
-        striker_mask = self.color_mask(frame, self.color_orange)
+        return np.array([
+                p2[0], (p2[0]-p1[0])/elapsed,
+                p2[1], (p2[1]-p1[1])/elapsed,
+                p2[2], (p2[2]-p1[2])/elapsed,
+                ])
         
-        puck_loc = self.find_centroids(puck_mask)
-        striker_locs = self.find_centroids(striker_mask, 2)
         
-        p_pos = get_pos(puck_loc[0])
-        if striker_locs[0][1]<striker_locs[1][1]:
-            s1_pos = get_pos(striker_locs[0])
-            s2_pos = get_pos(striker_locs[1])
-        else:
-            s1_pos = get_pos(striker_locs[0])
-            s2_pos = get_pos(striker_locs[1])            
-        
-        return [p_pos, s1_pos, s2_pos]
--- robots.py
+++ robots.py
@@ -1,4 +1,6 @@
 import numpy as np
+import RPi.GPIO as GPIO
+import time
 
 class DiscreteActionBotSim:
     """
@@ -37,3 +39,65 @@ class DiscreteActionBot:
     """
     Model of robot dynamics in discrete action space
     """
+    def __init__(self):
+        # consts
+        self.pulse_up = 0.125
+        self.pulse_down = 0.18
+        self.pulse_left = 0.075
+        self.pulse_right = 0.09
+        
+        # label pins
+        self.pins = {'x_up':11, 'x_down':12,
+                     'y1_up':16, 'y1_down':15,
+                     'y2_up':32, 'y2_down':31,
+                     }
+        # pin setup
+        GPIO.setmode(GPIO.BOARD)
+        for pin in self.pins.values():
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin,GPIO.HIGH)            
+        
+            
+    def execute(self, action):
+        """
+        execute action. blocks until action complete
+        """
+        x_sleep = 0
+        y_sleep = 0
+        start_time = time.time()
+        
+        if action[0]==0:
+            # move up
+            GPIO.output(self.pins['y1_up'],GPIO.LOW)
+            GPIO.output(self.pins['y2_up'],GPIO.LOW)
+            y_sleep = self.pulse_up
+
+        elif action[0]==2:
+            # move down
+            GPIO.output(self.pins['y1_down'],GPIO.LOW)
+            GPIO.output(self.pins['y2_down'],GPIO.LOW)
+            y_sleep = self.pulse_down
+
+        if action[1]==0:
+            # move left
+            GPIO.output(self.pins['x_down'],GPIO.LOW)
+            x_sleep = self.pulse_left
+        
+        elif action[1]==2:
+            # move right
+            GPIO.output(self.pins['x_up'],GPIO.LOW)
+            x_sleep = self.pulse_right
+        
+        # kill x
+        time.sleep(x_sleep)
+        GPIO.output(self.pins['x_up'],GPIO.HIGH)
+        GPIO.output(self.pins['x_down'],GPIO.HIGH)
+        
+        # kill y
+        time.sleep(max(0,y_sleep-x_sleep))
+        GPIO.output(self.pins['y1_up'],GPIO.HIGH)
+        GPIO.output(self.pins['y2_up'],GPIO.HIGH)
+        GPIO.output(self.pins['y1_down'],GPIO.HIGH)
+        GPIO.output(self.pins['y2_down'],GPIO.HIGH)
+        
+            

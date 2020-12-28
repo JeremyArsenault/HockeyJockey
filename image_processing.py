1e47fc5f862be9eaace3415483f7fb4226de9408
Jeremy Arsenault <arsena21@msu.edu>
2020-12-27 22:58:24 -0500

    get_state smoothing

--- image_processing.py
+++ image_processing.py
@@ -82,8 +82,8 @@ class Camera:
     def find_centroids(self, img, n=1):
         """
         Detect n largest contours of mask and find centers
-        If no objects detected, return center of table
-        :return: size sorted centriod list
+        If no objects detected, return None
+        :return: size sorted centriod list, True if all objects were detected
         """
         # Find contours
         contours = cv2.findContours(np.uint8(img), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -100,16 +100,18 @@ class Camera:
             areas.append(cv2.contourArea(c))
             
         # Make sure we have enough contours
+        detected=True
         while len(areas)<n:
-            centers.append([self.rescaled_dim[0]/2, self.rescaled_dim[1]/2])
+            centers.append(None)
             areas.append(0)
+            detected=False
             
         # Find top n sorted contours
         sorted_centers = []
         for i in np.argsort(-1*np.array(areas))[:n]:
             sorted_centers.append(centers[i])
             
-        return np.array(sorted_centers)
+        return np.array(sorted_centers), detected
 
 
     def set_corners(self):
@@ -119,8 +121,10 @@ class Camera:
         self.flush_buffer()
         _, frame = self.cam.read()
         corner_mask = self.color_mask(frame, self.color_pink, thresh=30, blur=5)
-        pts = self.find_centroids(corner_mask, 4)
-        
+        pts, detected = self.find_centroids(corner_mask, 4)
+        if not detected:
+            raise Exception("Failed to detect all four table corners")
+            
         # put pts in correct order
         corners = np.zeros((4, 2))
         s = pts.sum(axis = 1)
@@ -137,6 +141,8 @@ class Camera:
         convert location of centroid into absolute position in meters
         :return: position
         """
+        if loc is None:
+            return None
         pos_y = -(self.table_length/self.rescaled_dim[1])*(loc[1]-self.rescaled_dim[1]/2)
         pos_x = (self.table_width/self.rescaled_dim[0])*(loc[0]-self.rescaled_dim[0]/2)
         return [pos_x, pos_y]
@@ -144,7 +150,7 @@ class Camera:
     def get_pos(self, frame):
         """
         find position state vector or everything on table
-        pos (x,y)
+        pos (x,y) / None
         :return: [puck pos, s1 pos, s2 pos]
         """
         frame = self.perspective_shift(frame)
@@ -152,38 +158,65 @@ class Camera:
         puck_mask = self.color_mask(frame, self.color_green, thresh=13)
         striker_mask = self.color_mask(frame, self.color_orange, thresh=25, blur = 5)
         
-        puck_loc = self.find_centroids(puck_mask)
-        striker_locs = self.find_centroids(striker_mask, 2)
+        puck_loc, _ = self.find_centroids(puck_mask)
+        striker_locs, _ = self.find_centroids(striker_mask, 2)
         
         p_pos = self.abs_to_meter(puck_loc[0])
-        if striker_locs[0][1]<striker_locs[1][1]:
-            s1_pos = self.abs_to_meter(striker_locs[0])
-            s2_pos = self.abs_to_meter(striker_locs[1])
+        # cases: (pos,pos), (pos,None), (None,None)
+        if striker_locs[0] is not None:
+            pos_1 = self.abs_to_meter(striker_locs[0])
+            pos_2 = self.abs_to_meter(striker_locs[1])
+            s1_pos, s2_pos = pos_1, pos_2 if pos_1[1]<0 else pos_2, pos_1
         else:
-            s1_pos = self.abs_to_meter(striker_locs[0])
-            s2_pos = self.abs_to_meter(striker_locs[1])            
+            s1_pos, s2_pos = None, None          
         
-        return np.array([p_pos, s1_pos, s2_pos])
+        return [p_pos, s1_pos, s2_pos]
         
-    def get_state(self):
+    def get_state(self, time, frames=4):
         """
-        Capture frame and find position state vector or everything on table
+        Capture frame and find state vector of everything on the table.
+        :frames: number of frames captured to determine velocity
         :return: [puck pos, s1 pos, s2 pos]
+                 returns zeros if no history was recoverable 
         """
+        if frames<2:
+            raise ValueError('Needs at least 2 frames to determine velocity')
         self.flush_buffer()
-        _, frame1 = self.cam.read()
-        t = time.time()
-        p1 = self.get_pos(frame1)
+        start_t = time.time()
         
-        _, frame2 = self.cam.read()
-        elapsed = time.time()-t
-        t = time.time()
-        p2 = self.get_pos(frame2)
-        
-        return np.array([
-                p2[0], (p2[0]-p1[0])/elapsed,
-                p2[1],
-                p2[2],
-                ])
+        # time this to make sure we aren't blocking on get_pos for too long
+        puck_history = []
+        time_history = []
+        p_pos, p_vel = [0,0], [0,0]
+        s1_pos, s2_pos = [0,0], [0,0]
+        for i in range(frames):
+            _, frame = self.cam.read()
+            t = time.time()-start_t
+            p = self.get_pos(frame)
+            
+            if p[0] is not None:
+                puck_history.append(p[0])
+                time_history.append(t)
+            # choose last nonzero striker locations
+            if p[1] is not None:
+                s1_pos = p[1]
+            if p[2] is not None:
+                s2_pos = p[2]
+            
+        # estimate puck position at current time
+        if len(puck_history)==1:
+            p_pos = puck_history[0]
+        else:
+            # do linear regression
+            a = np.array([[t,1] for t in time_history])
+            b = np.array(puck_history)
+            m = np.linalg.lstsq(a, b, rcond=None)[0]
+            
+            t = np.array([[time.time()-start_t, 1]])
+            p_pos = np.dot(t,m)[0]
+            p_vel = m[:,0]
+            
+        return np.array([p_pos, p_vel, s1_pos, s2_pos])
+
         
         
